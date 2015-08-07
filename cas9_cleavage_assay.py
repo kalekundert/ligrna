359a20064e3ce4ad7f117e554b26714a09249254
Kale Kundert <kale@thekunderts.net>
2015-08-06 18:12:32 -0700

    Modify the protocol based on today's experience.
    Add notes about how to answer the robot's questions and remove the
    loading dye from the quenching master mix.

--- cas9_cleavage_assay.py
+++ cas9_cleavage_assay.py
@@ -38,8 +38,8 @@ steps = []
 args = docopt.docopt(__doc__)
 using_robot = args['--robot']
 num_reactions = eval(args['<reactions>'])
-master_mix = num_reactions * (1 + float(args['--extra'] or 0) / 100)
-scale = lambda ref, name: (ref * master_mix, name)
+num_scaled_reactions = num_reactions * (1 + float(args['--extra'] or 0) / 100)
+num_sgrnas = num_reactions // 2
 
 ## Calculate how much of each reagent will be needed.
 
@@ -50,20 +50,31 @@ theo = float(args['--theo'])
 water = 27 - sgrna - cas9 - dna - theo
 cas9_mm = 30 - theo - sgrna - dna
 
-cas9_reagents = [  # (no fold)
-        scale(water, "nuclease-free water"),
-        scale(3.0, "10x reaction buffer"),
-        scale(cas9, "1 μM Cas9 (NEB)"),
-]
-kag_reagents = [  # (no fold)
-        scale(0.337, "Proteinase K (NEB)"),
-        scale(3.371, "Buffer P1 with RNase A (Qiagen)"),
-        scale(6.292, "10x Orange G loading buffer"),
-]
+def scale(*reagents):   # (no fold)
+    volume_per_reaction = sum(amount for amount, x in reagents)
 
-cas9_volume = sum(amount for amount, x in cas9_reagents)
-kag_volume = sum(amount for amount, x in kag_reagents)
-max_volume = sum(x for x, y in cas9_reagents + kag_reagents) 
+    if using_robot:
+        scaled_volume = num_reactions * volume_per_reaction + 22
+    else:
+        scaled_volume = num_scaled_reactions * volume_per_reaction
+
+    scaled_reagents = [
+            (ref * scaled_volume / volume_per_reaction, name)
+            for ref, name in reagents]
+
+    return scaled_reagents, scaled_volume
+
+cas9_reagents, cas9_volume = scale(
+        (water, "nuclease-free water"),
+        (3.0, "10x reaction buffer"),
+        (cas9, "1 μM Cas9 (NEB)"),
+)
+ka_reagents, ka_volume = scale(
+        (0.337, "Proteinase K (NEB)"),
+        (3.371, "Buffer P1 with RNase A (Qiagen)"),
+)
+
+max_volume = sum(x for x, y in cas9_reagents + ka_reagents) 
 max_digits = int(math.ceil(math.log10(max_volume)))
 row = '{{:{}.2f}} μL  {{}}'.format(max_digits + 2)
 
@@ -73,7 +84,7 @@ steps.append("""\
 Prepare fresh 30 mM theophylline. 
 
 30 mM Theophylline
-==============================
+===================================
  x≈5 mg  theophylline
 185x μL  nuclease-free water
 
@@ -92,11 +103,11 @@ if using_robot:
 
     steps.append('\n'.join([
 "Prepare the Cas9 master mix:\n".format(args['<reactions>']),
-"Cas9 Master Mix for {:.1f} reactions".format(master_mix),
-30 * '='] + [
+"Cas9 Master Mix for {} reactions".format(num_reactions),
+35 * '='] + [
     row.format(amount, reagent)
     for amount, reagent in cas9_reagents] + [
-30 * '-',
+35 * '-',
 row.format(cas9_volume, "total master mix"),
 '',
 ]))
@@ -129,28 +140,43 @@ C2: sgRNAs in a plastic 96-well rack with a 2 mm
     # Run the Cas9 reactions.
 
     steps.append("""\
-Run the method.  Provide the volumes it asks for.  
+Run the method.  Answer the robot's questions as 
+follows:
+
+- The number of sgRNAs to test: {num_sgrnas}
+
+- The number of reactions to test: {num_reactions}
+
+- Levelsensor settings:
+  [ ] Levels
+  [x] Tips
+  [ ] Locations
+
+- Fill in volumes for each reagent.  Specify the 
+  minimum values for the Cas9 and KA mixes.  Make 
+  sure everything else agrees with the defaults.
+
 Watch to make sure that liquid is actually being 
 pipetted for each step.
-""")
+""".format(**locals()))
 
     # Quench the Cas9 reactions.
 
     steps.append('\n'.join(["""\
-Just before the reaction finishes, prepare the KAG 
-master mix:
+Just before the 1 hour 37°C incubation finishes, 
+prepare the KA master mix:
 """,
-"3x KAG Master Mix for {:.1f} reactions".format(master_mix),
-30 * '='] + [
+"≈9x KA Master Mix for {} reactions".format(num_reactions),
+35 * '='] + [
     row.format(amount, reagent)
-    for amount, reagent in kag_reagents] + [
-30 * '-',
-row.format(kag_volume, "total master mix"),
+    for amount, reagent in ka_reagents] + [
+35 * '-',
+row.format(ka_volume, "total master mix"),
 '',
 ]))
     steps.append("""\
-Put the KAG master mix in position B1;5 when the 
-robot asks for it, then continue the method.
+Put the KA master mix in position B1-5 when the 
+robot asks for it, then finish running the method.
 """)
 
 ## Run the Cas9 reactions (by hand).
@@ -163,19 +189,19 @@ Setup {} Cas9 reactions.  Add theophylline,
 sgRNA, and Cas9 master mix in that order to each 
 reaction (as appropriate).
 """.format(args['<reactions>']),
-'Cas9 Master Mix for {:.1f} reactions'.format(master_mix),
-30 * '='] + [
+'Cas9 Master Mix for {:.1f} reactions'.format(num_scaled_reactions),
+35 * '='] + [
     row.format(amount, reagent)
     for amount, reagent in cas9_reagents] + [
-30 * '-',
+35 * '-',
 row.format(cas9_volume, 'total master mix'),
 '',
 'Each Cas9 Reaction',
-30 * '=',
+35 * '=',
 row.format(theo, "30 mM theophylline (or water)"),
 row.format(sgrna, "300 nM sgRNA (or water)"),
 row.format(cas9_mm, 'Cas9 mix (or 3 μL buffer + {:g} μL water)'.format(cas9_mm - 3)),
-30 * '-',
+35 * '-',
 row.format(dna, "30 nM target DNA (or water)"),
 '',
 ]))
@@ -195,13 +221,13 @@ Incubate at 37°C for 1 hour.
     # Quench the Cas9 reactions.
 
     steps.append('\n'.join(["""\
-Add 10.0 μL of 2x KAG master mix to each 
+Add 3.7 μL of ≈9x KA master mix to each 
 reaction.  Prepare immediately before adding:
 """,
-"3x KAG Master Mix for {:.1f} reactions".format(master_mix),
-30 * '='] + [
+"≈9x KA Master Mix for {:.1f} reactions".format(num_scaled_reactions),
+35 * '='] + [
     row.format(amount, reagent)
-    for amount, reagent in kag_reagents] + [
+    for amount, reagent in ka_reagents] + [
 '',
 ]))
     steps.append("""\
@@ -210,6 +236,11 @@ min, then hold at 12°C.
 """) 
 
 ## Analyze the reactions.
+
+steps.append("""\
+Add 6.3 μL 6x Orange G loading buffer to each 
+reaction.  Flick to mix and spin to collect.
+""")
 
 steps.append("""\
 Load on a 2% TAE/agarose/GelRed gel and run at 
