4a9c87b9d3c6ece516df95ee37dc872b8afafd5d
Kale Kundert <kale@thekunderts.net>
2016-02-23 15:10:34 -0800

    Design cloning primers for rational designs.
    Cloning is done by inverse PCR.

---
+++ clone_into_wt.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+"""
+Pick inverse PCR primers that can be used to clone a rationally designed sgRNA 
+into a plasmid already harboring the wildtype sgRNA.
+
+Usage:
+    clone_into_wt.py <designs>... [options]
+
+Options:
+    -s, --spacer <name>     [default: rfp]
+        The spacer sequence of the wildtype plasmid.  For some designs, this 
+        doesn't matter because the primers won't overlap the spacer.  But for 
+        other designs, particularly upper stem designs, different primers are 
+        needed for each spacer.
+
+    -t, --tm <celsius>      [default: 60]
+        The desired melting temperature for the primers.
+
+    -v, --verbose
+        Show extra debugging output.
+        
+"""
+
+def design_cloning_primers(name, spacer, tm=60, verbose=False):
+    import sgrna_sensor, itertools
+
+    wt = sgrna_sensor.from_name('wt', target=spacer)
+    design = sgrna_sensor.from_name(name, target=spacer)
+
+    # Find where the design differs from the wildtype sgRNA:
+
+    mismatch_5 = None
+    mismatch_3 = None
+
+    for i in itertools.count():
+        if wt.dna[i] != design.dna[i]:
+            mismatch_5 = i
+            break
+
+    for j in itertools.count(1):
+        if wt.dna[-j] != design.dna[-j]:
+            mismatch_3 = -j + 1
+            break
+
+    if verbose:
+        print("5' matching region (wt):", wt.dna[:mismatch_5])
+        print("5' matching region (design):", design.dna[:mismatch_5])
+        print()
+        print("mismatched region (wt):", wt.dna[mismatch_5:mismatch_3])
+        print("mismatched region (design):", design.dna[mismatch_5:mismatch_3])
+        print()
+        print("3' matching region (wt):", wt.dna[mismatch_3:])
+        print("3' matching region (design):", design.dna[mismatch_3:])
+        print()
+
+    wt_5 = wt.dna[:mismatch_5]
+    wt_3 = wt.dna[mismatch_3:]
+    insert = design.dna[mismatch_5:mismatch_3]
+
+    # Design the part of the primers that will anneal with the wildtype plasmid.
+
+    overlaps_5 = [wt_5[-j:] for j in range(1, len(wt_5))]
+    overlap_5, tm_5 = pick_primer_with_best_tm(overlaps_5, tm)
+
+    overlaps_3 = [wt_3[:i] for i in range(1, len(wt_5))]
+    overlap_3, tm_3 = pick_primer_with_best_tm(overlaps_3, tm)
+
+    if verbose:
+        print("5' overlap:", overlap_5)
+        print("5' overlap Tm:", tm_5)
+        print("5' overlap GC%:", 100 * sum(x in 'GC' for x in overlap_5) / len(overlap_5))
+        print("5' overlap len:", len(overlap_5))
+        print()
+        print("3' overlap:", overlap_3)
+        print("3' overlap Tm:", tm_3)
+        print("3' overlap GC%:", 100 * sum(x in 'GC' for x in overlap_3) / len(overlap_3))
+        print("3' overlap len:", len(overlap_3))
+        print()
+
+    # Design the part of the primers that will contain the aptamer insert.
+
+    dlen = len(overlap_5) - len(overlap_3)
+    half = (len(insert) - dlen) // 2
+
+    overhang_5 = insert[:half].lower()
+    overhang_3 = insert[half:].lower()
+
+    if verbose:
+        print("5' overhang:", overlap_5)
+        print("3' overhang:", overlap_3)
+        print()
+
+    # Combine the two halves of each primer.  Print the overlap in uppercase and 
+    # the overhang in lowercase.  Reverse-complement the 5' primer.
+
+    primer_5 = reverse(complement(overlap_5 + overhang_5))
+    primer_3 = overhang_3 + overlap_3
+
+    name = '{}_{}'.format(spacer, design.underscore_name).upper()
+    name_5 = name + '_FOR'
+    name_3 = name + '_REV'
+
+    return {name_5: primer_5, name_3: primer_3}
+
+def print_cloning_primers(primers):
+    print("Number of oilgos:")
+    print(len(primers))
+    print()
+    print("Primer names (ready to copy into Elim form):")
+    for name in sorted(primers):
+        print(name)
+    print()
+    print("Primer sequences (ready to copy into Elim form):")
+    for name in sorted(primers):
+        print(primers[name])
+
+def pick_primer_with_best_tm(seqs, tm):
+    import primer3
+    seq_tms = [
+            (seq, primer3.calcTm(seq, tm_method='breslauer'))
+            for seq in seqs
+    ]
+    seq_tms.sort(key=lambda seq_tm: abs(seq_tm[1] - tm))
+    return seq_tms[0]
+
+def reverse(sequence):
+    return sequence[::-1]
+
+def complement(sequence):
+    complements = {
+            'a': 't',
+            'c': 'g',
+            'g': 'c',
+            't': 'a',
+            'A': 'T',
+            'C': 'G',
+            'G': 'C',
+            'T': 'A',
+    }
+    return ''.join(complements[x] for x in sequence)
+
+
+if __name__ == '__main__':
+    import docopt
+    args = docopt.docopt(__doc__)
+    tm = float(args['--tm'])
+    spacer = args['--spacer']
+    primers = {}
+
+    for design in args['<designs>']:
+        primers.update(
+                design_cloning_primers(
+                    design, spacer, tm=tm, verbose=args['--verbose']))
+
+    print_cloning_primers(primers)
+
