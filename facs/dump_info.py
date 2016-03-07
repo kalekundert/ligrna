8e7f22cc839a638425c6cb0c9192bcc7d536acba
Kale Kundert <kale@thekunderts.net>
2016-03-07 08:54:44 -0800

    Replace the <experiment> argument with the more flexible <keyword> argument.

--- facs/dump_info.py
+++ facs/dump_info.py
@@ -4,26 +4,28 @@
 Print a basic statistical description of all the wells in a given experiment.
 
 Usage:
-    dump_info.py <yml_path> [<experiment>]
+    dump_info.py <yml_path> [<keyword>]
 """
 
 import fcmcmp, docopt
 from pprint import pprint
 
 args = docopt.docopt(__doc__)
-
-if args['<experiment>']:
-    experiments = [fcmcmp.load_experiment(args['<yml_path>'], args['<experiment>'])]
-else:
-    experiments = fcmcmp.load_experiments(args['<yml_path>'])
+experiments = fcmcmp.load_experiments(args['<yml_path>'])
+keyword = args['<keyword>']
 
 for experiment, condition, well in fcmcmp.yield_wells(experiments):
-    print('Experiment:')
-    pprint(experiment)
-    print()
-    print(well, end=':\n')
-    print()
-    print(well.data.describe())
-    print()
+    if keyword is None or \
+            keyword in experiment['label'] or \
+            keyword == condition or \
+            keyword == well.label:
+
+        print('Experiment:')
+        pprint(experiment)
+        print()
+        print(well, end=':\n')
+        print()
+        print(well.data.describe())
+        print()
     
 
