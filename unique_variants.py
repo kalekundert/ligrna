d0cd3ec61177a1c6b9c73c7894730ebbf5b2ef3e
Kale Kundert <kale@thekunderts.net>
2016-04-15 13:19:42 -0700

    Move to a simpler, linear, YAML-based format.
    Also polish the user interface.

--- unique_variants.py
+++ unique_variants.py
@@ -1,27 +1,45 @@
 #!/usr/bin/env python3
 
 """\
+Calculate how many unique library members remain in each step of a screen.  
+This information can inform how long and how accurate sorting steps should be.
+
 Usage:
     unique_variants.py <protocol> [options]
     unique_variants.py <num_items> <num_picked>... [options]
 
-Options:
-    -f --fraction
-    -n --count
-    -t --sort-time <percent,time> [default: ??]
-    -T --sort-times-below <percent,time> [default: 3h]
-    -e --event-rate <evt_sec> [default: 10000]
-"""
+Arguments:
+    <protocol>
+        A YAML-formatted file describing all the steps in a library screen.  
+        The number of individuals kept after each step is specified.
+        
+    <num_items>
+        The theoretical complexity of the library being picked from.
 
-"""
--T 2h
--t 2h
--t 120m
--t 99%
--T 99%
+    <num_picked>
+        The number of individuals you picked from the library.
+
+Options:
+    -e --event-rate <evt_sec>
+        Report how long it would take to screen '--fraction-wanted' of the 
+        entire library on the BD FACSAria II with the given event rate.  The 
+        fact that some cells will be double-counted and that some cells won't 
+        survive sorting will be accounted for.  Note that you should think of 
+        event rate as a proxy for sorting accuracy.
+
+    -f --fraction-wanted <percent>  [default: 95]
+        If you specified '--event-rate', what fraction of the library do you 
+        want to screen?  63% corresponds to 1x coverage, roughly speaking.
+
+    -s --survival-rate <percent>  [default: 60]
+        If you specified '--event-rate', what fraction of cells survive 
+        sorting?  I've measured this in two ways (plating and flow cytometry) 
+        and gotten ~60% by both metrics.  However, it may be that you could 
+        optimize conditions to increase cell viability.
 """
 
 from pprint import pprint
+inf = float('inf')
 
 def fraction_picked(num_items, num_picked):
     return 1 - ((num_items - 1) / num_items)**num_picked
@@ -29,11 +47,55 @@ def fraction_picked(num_items, num_picked):
 def unique_items(num_items, num_picked):
     return num_items * fraction_picked(num_items, num_picked)
 
-def cast_to_int(x):
+def sort_time(num_items, fraction_wanted, event_rate=10000, survival_rate=0.6):
+    import numpy as np
+    import scipy.stats
+
+    # Predict how efficient the sorting will be.  I've observed that the linear 
+    # correlation between event rate and sorting efficiency is pretty good, 
+    # although the efficiency also fluctuates by ~10% depending on how common 
+    # the cells you're sorting for are.
+
+    event_rates_to_efficiencies = {
+        47668: 0.08, # 20160331_optimize_sorting_speed
+        26166: 0.42,
+         9575: 0.73,
+         3098: 0.90,
+        49921: 0.06,
+        26267: 0.39,
+         9961: 0.72,
+         3233: 0.90,
+        53380: 0.10,
+        30071: 0.49,
+        10233: 0.81,
+         2917: 0.91,
+        53380: 0.16,
+        30071: 0.57,
+        10233: 0.84,
+         2917: 0.94,
+         1135: 0.96, # 20160414_screen_rb
+         1348: 0.96,
+    }
+
+    event_rates = np.array(list(event_rates_to_efficiencies.keys()))
+    efficiencies = np.array(list(event_rates_to_efficiencies.values()))
+    m, b, _, _, _ = scipy.stats.linregress(event_rates, efficiencies)
+    efficiency = m * event_rate + b
+
+    # Calculate how long it will take to collect the given fraction of the 
+    # library, accounting for the fact that some cells will be double-counted 
+    # and some cells won't survive sorting.
+
+    counting = np.log(1 - fraction_wanted) / np.log((num_items - 1) / num_items)
+    inv_min = survival_rate * efficiency * event_rate * 60
+    sort_time = int(counting / inv_min)
+    return '{}h{:02d}'.format(sort_time // 60, sort_time % 60)
+
+def cast_to_number(x):
     try:
         return int(x)
     except:
-        return int(eval(x))
+        return eval(x)
 
 def percent(x, precision=2):
     return '{1:.{0}f}%'.format(precision, 100 * step.fraction_picked)
@@ -42,6 +104,15 @@ def scientific_notation(x, precision=2):
     from math import log10
     exponent = int(log10(x))
     mantissa = x / 10**exponent
+    mantissa_str = '{0:.{1}f}'.format(mantissa, precision)
+
+    # Handle the corner case where the mantissa rounds up to 10, in which case 
+    # we should increment the exponent by one.
+    if mantissa_str.startswith('10'):
+        exponent += 1
+        mantissa /= 10
+        mantissa_str = '{0:.{1}f}'.format(mantissa, precision)
+
     superscripts = str.maketrans('-0123456789', '⁻⁰¹²³⁴⁵⁶⁷⁸⁹')
     superscript_exponent = str(exponent).translate(superscripts)
     return '{1:.{0}f}×10{2}'.format(precision, mantissa, superscript_exponent)
@@ -59,6 +130,10 @@ class PickStep:
 
     @property
     def num_items(self):
+        # If the specific number of items wasn't specified, return None.
+        if self._num_items is None:
+            return None
+
         # If there was a selection step before this one, see how many unique 
         # items it yielded.
         try:
@@ -79,11 +154,11 @@ class PickStep:
         # If none of these conditions apply, return the underlying attribute, 
         # converted to a number (e.g. via ``eval`` for strings) if necessary.
 
-        return cast_to_int(self._num_items)
+        return cast_to_number(self._num_items)
 
     @property
     def num_picked(self):
-        return cast_to_int(self._num_picked)
+        return cast_to_number(self._num_picked)
 
     @property
     def unique_items(self):
@@ -120,43 +195,34 @@ class SortStep(PickStep):
 
     @property
     def num_sampled(self):
-        return cast_to_int(self._num_sampled)
+        return cast_to_number(self._num_sampled)
 
 
 
-def parse_num_file(path):
+def steps_from_yaml(path):
+    import yaml
+
     with open(path) as file:
-        blocks = file.read().strip().split('\n\n')
+        records = yaml.load(file)
 
-    all_steps = []
-    last_step_with_indent = {-1: None}
+    steps = []
 
-    for block in blocks:
-        leading_space = len(block) - len(block.lstrip())
-        indent = leading_space // 2
-        last_step = last_step_with_indent[indent-1]
+    for record in records:
+        num_items = steps[-1] if steps else None
 
-        *labels, num_picked = [x.strip() for x in block.splitlines()]
-        label = '\n'.join(labels)
-
-        if 'of' not in num_picked:
-            step = PickStep(last_step, num_picked)
+        if 'picked' in record:
+            num_picked = record['picked']
+            step = PickStep(num_items, num_picked)
+        elif 'sorted' in record:
+            num_picked, num_sampled = record['sorted'].split(' of ')
+            step = SortStep(num_items, num_picked, num_sampled) 
         else:
-            num_picked, num_sampled = num_picked.split(' of ')
-            step = SortStep(last_step, num_picked, num_sampled)
+            raise SyntaxError("Every step must specify either 'picked' or 'sorted'.")
 
-        all_steps.append(step)
-        last_step_with_indent[indent] = step
+        step.name = record.get('step', '')
+        steps.append(step)
 
-    return all_steps
-
-def most_recent(steps):
-    most_recent = steps[:]
-    for step in steps:
-        try: most_recent.remove(step.previous_step)
-        except ValueError: pass
-    return most_recent
-
+    return steps
 
 if __name__ == '__main__':
     import docopt, yaml, tabulate
@@ -168,39 +234,30 @@ if __name__ == '__main__':
                 PickStep(args['<num_items>'], num_picked)
                 for num_picked in args['<num_picked>']
         ]
-
     if args['<protocol>']:
-        steps = parse_num_file(args['<protocol>'])
-        steps = most_recent(steps)
+        steps = steps_from_yaml(args['<protocol>'])
 
     table = []
     for step in steps:
         row = []
         table.append(row)
 
-        if args['--count']:
-            row.append(scientific_notation(step.unique_items))
-        if args['--fraction']:
-            row.append(percent(step.fraction_picked))
-        if args['--sort-time']:
-            raise NotImplementedError
-        if args['--sort-times-below']:
-            raise NotImplementedError
+        if any(x.name for x in steps):
+            row.append(step.name)
+
+        row.append(scientific_notation(step.unique_items))
+        row.append(percent(step.fraction_picked))
+
+        if args['--event-rate']:
+            row.append(sort_time(
+                step.unique_items,
+                eval(args['--fraction-wanted']) / 100,
+                eval(args['--event-rate']),
+                eval(args['--survival-rate']) / 100,
+            ))
 
     print(tabulate.tabulate(table, tablefmt='plain'))
 
 
 
-
-# Parse yaml file, pick nodes to report, report fraction/num unique for each.
-#
-#
-# Problem: This is really a tree.  For example, I'm using the same MG1655 
-# transformation for lots of sorts.  For another, I collected two "dead" 
-# populations from the same culture.  When I branch like this, I have to 
-# duplicate everything up to the branch point.
-
-# How do I know which numbers to show?  By default, show all leaves?  Allow 
-# user to select nodes by partial string match.  Also allow the user to show 
-# all leaves below nodes matched by a partial string.
 
