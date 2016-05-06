05b3286690c2fcafbe011f1e5f880e9e124852ca
Kale Kundert <kale@thekunderts.net>
2016-05-06 14:25:31 -0700

    Add a option to specify how much was plated.

--- count_transformants.py
+++ count_transformants.py
@@ -14,7 +14,12 @@ Usage:
     count_transformed_cells.py <cfus>... [options]
 
 Options:
-    -v --volume <μL>    [default: 1000]
+    -p --plate-volume <μL>          [default: 20]
+        The volume of cells that were plated for each dilution.  By default 
+        this is 20 μL, which is a good amount for titering 16 measurements on a 
+        single plate.
+        
+    -r --recovery-volume <μL>       [default: 1000]
         The total volume of recovered cells.  By default, this is 1 mL, which 
         is the amount of SOC I typically add after electroporation.  However, 
         the volume should technically also include the volume of the competent 
@@ -25,20 +30,24 @@ import docopt
 from pylab import *
 
 args = docopt.docopt(__doc__)
-cfus = array([int(n) for n in args['<cfus>']])
-volume = float(args['--volume'])
-dilutions = arange(len(cfus))
-transformants = volume * (200 / 2) * (200 / 20)**dilutions * (cfus / 100)
+cfus = array([eval(n) for n in args['<cfus>']])
+plate_volume = float(args['--plate-volume'])
+recovery_volume = float(args['--recovery-volume'])
+dilutions = (200 / 2) * (200 / 20)**arange(len(cfus))
+transformants = recovery_volume * dilutions * (cfus / plate_volume)
+num_data = len(cfus[cfus != 0])
 
 print('Dilution  # CFU  # transformants')
 print('========  =====  ===============')
-for i in dilutions:
-    print('1e-{:<5d}  {:>5d}  {:>14.2e}'.format(
-        i + 3,
-        cfus[i],
-        transformants[i],
-    ))
+for i in range(len(cfus)):
+    if cfus[i] > 0:
+        print('1e-{:<5d}  {:>5d}  {:>14.2e}'.format(
+            i + 3,
+            cfus[i],
+            transformants[i],
+        ))
 
-print("Weighted average: {:13.2e}".format(
-    np.average(transformants, weights=cfus)))
+if num_data > 1:
+    print("Weighted average: {:13.2e}".format(
+        np.average(transformants, weights=cfus)))
 
