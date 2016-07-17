4434a8f1de0eff148d499a60c6f463682061cf7d
Kale Kundert <kale@thekunderts.net>
2016-07-17 11:30:06 -0700

    Setup parameters for the real design run.

--- run_mh_design.sh
+++ run_mh_design.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 # Clean up old files
-mkdir -p stdout spacers no_spacers
-rm -f stdout/* spacers/*.tsv no_spacers/*.tsv
+rm -rf results stdout
+mkdir -p results/{no,example,random}_spacers stdout
 
 # Submit the design jobs
-qsub -t 1-30  -v "ADDAPT_ARGS=inputs/mh_6.yml -n16000 inputs/scorefxn.yml inputs/fast_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_6" sgrna_mc
-qsub -t 1-30  -v "ADDAPT_ARGS=inputs/mh_7.yml -n16000 inputs/scorefxn.yml inputs/fast_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_7" sgrna_mc
-qsub -t 1-100 -v "ADDAPT_ARGS=inputs/mh_6.yml -n16000 inputs/scorefxn.yml inputs/slow_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_6" sgrna_mc
-qsub -t 1-100 -v "ADDAPT_ARGS=inputs/mh_7.yml -n16000 inputs/scorefxn.yml inputs/slow_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_7" sgrna_mc
-qsub -t 1-100 -v "ADDAPT_ARGS=inputs/mh_6.yml -n16000 inputs/scorefxn.yml inputs/slow_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_6" sgrna_mc
-qsub -t 1-100 -v "ADDAPT_ARGS=inputs/mh_7.yml -n16000 inputs/scorefxn.yml inputs/slow_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_7" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_6" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_7" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_6" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_7" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_6" sgrna_mc
+qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_7" sgrna_mc
