38df7e0bb107342b7d5fa7a8266ebfd0d52c1a69
Kale Kundert <kale@thekunderts.net>
2016-07-18 16:05:17 -0700

    Don't forget to mkdir the output directory.

--- run_mh_design.sh
+++ run_mh_design.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Clean up old files
 rm -rf results stdout
-mkdir -p results/{no,example,random}_spacers stdout
+mkdir -p results/mh_design/{no,example,random}_spacers stdout
 
 # Submit the design jobs
 qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_6" sgrna_mc
