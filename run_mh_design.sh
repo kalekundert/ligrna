86d48fb44926e83c58d5c9cdab58b2229b9a0798
Kale Kundert <kale@thekunderts.net>
2016-07-18 15:56:13 -0700

    Tweak submission scripts.

--- run_mh_design.sh
+++ run_mh_design.sh
@@ -6,9 +6,9 @@ rm -rf results stdout
 mkdir -p results/{no,example,random}_spacers stdout
 
 # Submit the design jobs
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_6" sgrna_mc
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/no_spacers/mh_7" sgrna_mc
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_6" sgrna_mc
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/example_spacers/mh_7" sgrna_mc
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_6" sgrna_mc
-qsub -t 1-50 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/random_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_7" sgrna_mc
