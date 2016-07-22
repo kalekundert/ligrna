4778369e2e48e4899c6631d4e1669d89e0abe700
Kale Kundert <kale@thekunderts.net>
2016-07-22 15:53:49 -0700

    Try a score function that explicitly misfolds the nexus.

--- run_mh_design.sh
+++ run_mh_design.sh
@@ -3,12 +3,19 @@ set -euo pipefail
 
 # Clean up old files
 rm -rf results stdout
-mkdir -p results/mh_design/{no,example,random}_spacers stdout
+mkdir -p results/{mh_design,misfold_nexus}/{no,example,random}_spacers stdout
 
 # Submit the design jobs
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_7" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_7" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/mh_design/example_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_7" sgrna_mc
+
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_6" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_7" sgrna_mc
