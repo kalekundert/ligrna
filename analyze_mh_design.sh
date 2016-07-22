b1b9b80c78dc1c22838fdba409b0f9658d16cf0e
Kale Kundert <kale@thekunderts.net>
2016-07-22 15:37:41 -0700

    Fix the path to the results.

--- analyze_mh_design.sh
+++ analyze_mh_design.sh
@@ -5,25 +5,25 @@ IFS=$'\n\t'
 mkdir -p figures/mh_design/{no,example,random}_spacers/{auto_corr,pick_seqs,seq_logo}
 
 echo "Making autocorrelation plots..."
-tools/auto_corr results/no_spacers/mh_6_001.tsv      -w 2000 -o figures/mh_design/no_spacers/auto_corr/mh_6.pdf
-tools/auto_corr results/no_spacers/mh_7_001.tsv      -w 2000 -o figures/mh_design/no_spacers/auto_corr/mh_7.pdf
-tools/auto_corr results/example_spacers/mh_6_001.tsv -w 2000 -o figures/mh_design/example_spacers/auto_corr/mh_6.pdf
-tools/auto_corr results/example_spacers/mh_7_001.tsv -w 2000 -o figures/mh_design/example_spacers/auto_corr/mh_7.pdf
-tools/auto_corr results/random_spacers/mh_6_001.tsv  -w 2000 -o figures/mh_design/random_spacers/auto_corr/mh_6.pdf
-tools/auto_corr results/random_spacers/mh_7_001.tsv  -w 2000 -o figures/mh_design/random_spacers/auto_corr/mh_7.pdf
+tools/auto_corr results/mh_design/no_spacers/mh_6_001.tsv      -w 2000 -o figures/mh_design/no_spacers/auto_corr/mh_6.pdf
+tools/auto_corr results/mh_design/no_spacers/mh_7_001.tsv      -w 2000 -o figures/mh_design/no_spacers/auto_corr/mh_7.pdf
+tools/auto_corr results/mh_design/example_spacers/mh_6_001.tsv -w 2000 -o figures/mh_design/example_spacers/auto_corr/mh_6.pdf
+tools/auto_corr results/mh_design/example_spacers/mh_7_001.tsv -w 2000 -o figures/mh_design/example_spacers/auto_corr/mh_7.pdf
+tools/auto_corr results/mh_design/random_spacers/mh_6_001.tsv  -w 2000 -o figures/mh_design/random_spacers/auto_corr/mh_6.pdf
+tools/auto_corr results/mh_design/random_spacers/mh_7_001.tsv  -w 2000 -o figures/mh_design/random_spacers/auto_corr/mh_7.pdf
 
 echo "Picking sequences..."
-tools/pick_seqs results/no_spacers/mh_6_???.tsv      -w 500 -vv -s > figures/mh_design/no_spacers/pick_seqs/mh_6.pdf
-tools/pick_seqs results/no_spacers/mh_7_???.tsv      -w 500 -vv -s > figures/mh_design/no_spacers/pick_seqs/mh_7.pdf
-tools/pick_seqs results/example_spacers/mh_6_???.tsv -w 500 -vv -s > figures/mh_design/example_spacers/pick_seqs/mh_6.pdf
-tools/pick_seqs results/example_spacers/mh_7_???.tsv -w 500 -vv -s > figures/mh_design/example_spacers/pick_seqs/mh_7.pdf
-tools/pick_seqs results/random_spacers/mh_6_???.tsv  -w 500 -vv -s > figures/mh_design/random_spacers/pick_seqs/mh_6.pdf
-tools/pick_seqs results/random_spacers/mh_7_???.tsv  -w 500 -vv -s > figures/mh_design/random_spacers/pick_seqs/mh_7.pdf
+tools/pick_seqs results/mh_design/no_spacers/mh_6_???.tsv      -w 500 -vv -s > figures/mh_design/no_spacers/pick_seqs/mh_6.txt
+tools/pick_seqs results/mh_design/no_spacers/mh_7_???.tsv      -w 500 -vv -s > figures/mh_design/no_spacers/pick_seqs/mh_7.txt
+tools/pick_seqs results/mh_design/example_spacers/mh_6_???.tsv -w 500 -vv -s > figures/mh_design/example_spacers/pick_seqs/mh_6.txt
+tools/pick_seqs results/mh_design/example_spacers/mh_7_???.tsv -w 500 -vv -s > figures/mh_design/example_spacers/pick_seqs/mh_7.txt
+tools/pick_seqs results/mh_design/random_spacers/mh_6_???.tsv  -w 500 -vv -s > figures/mh_design/random_spacers/pick_seqs/mh_6.txt
+tools/pick_seqs results/mh_design/random_spacers/mh_7_???.tsv  -w 500 -vv -s > figures/mh_design/random_spacers/pick_seqs/mh_7.txt
 
 echo "Making sequence logos..."
-tools/seq_logo results/no_spacers/mh_6_???.tsv      -w 500 -i -o figures/mh_design/no_spacers/seq_logo/mh_6.pdf
-tools/seq_logo results/no_spacers/mh_7_???.tsv      -w 500 -i -o figures/mh_design/no_spacers/seq_logo/mh_7.pdf
-tools/seq_logo results/example_spacers/mh_6_???.tsv -w 500 -i -o figures/mh_design/example_spacers/seq_logo/mh_6.pdf
-tools/seq_logo results/example_spacers/mh_7_???.tsv -w 500 -i -o figures/mh_design/example_spacers/seq_logo/mh_7.pdf
-tools/seq_logo results/random_spacers/mh_6_???.tsv  -w 500 -i -o figures/mh_design/random_spacers/seq_logo/mh_6.pdf
-tools/seq_logo results/random_spacers/mh_7_???.tsv  -w 500 -i -o figures/mh_design/random_spacers/seq_logo/mh_7.pdf
+tools/seq_logo results/mh_design/no_spacers/mh_6_???.tsv      -w 500 -o figures/mh_design/no_spacers/seq_logo/mh_6.pdf
+tools/seq_logo results/mh_design/no_spacers/mh_7_???.tsv      -w 500 -o figures/mh_design/no_spacers/seq_logo/mh_7.pdf
+tools/seq_logo results/mh_design/example_spacers/mh_6_???.tsv -w 500 -o figures/mh_design/example_spacers/seq_logo/mh_6.pdf
+tools/seq_logo results/mh_design/example_spacers/mh_7_???.tsv -w 500 -o figures/mh_design/example_spacers/seq_logo/mh_7.pdf
+tools/seq_logo results/mh_design/random_spacers/mh_6_???.tsv  -w 500 -o figures/mh_design/random_spacers/seq_logo/mh_6.pdf
+tools/seq_logo results/mh_design/random_spacers/mh_7_???.tsv  -w 500 -o figures/mh_design/random_spacers/seq_logo/mh_7.pdf
