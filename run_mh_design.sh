c717d15133b8c481c939cc217b3cdb941acd3c88
Kale Kundert <kale@thekunderts.net>
2016-07-28 13:59:04 -0700

    Make mh designs for in vitro testing.
    Specifically, don't randomize the nexus.

--- run_mh_design.sh
+++ run_mh_design.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env sh
 set -euo pipefail
 
-# Clean up old files
-rm -rf results stdout
-mkdir -p results/{mh_design,misfold_nexus}/{no,example,random}_spacers stdout
-
 # Submit the design jobs
 #qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_6" sgrna_mc
 #qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/mh_design/random_spacers/mh_7" sgrna_mc
@@ -13,9 +9,13 @@ mkdir -p results/{mh_design,misfold_nexus}/{no,example,random}_spacers stdout
 #qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_6.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_6" sgrna_mc
 #qsub -t 1-25 -v "ADDAPT_ARGS=inputs/mh_7.yml -n10000 inputs/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/mh_design/no_spacers/mh_7" sgrna_mc
 
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_7" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_7" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_6" sgrna_mc
-qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/random_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/misfold_nexus/example_spacers/mh_7" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_6.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_6" sgrna_mc
+#qsub -t 1-25 -v "ADDAPT_ARGS=inputs/misfold_nexus/mh_7.yml -n10000 inputs/misfold_nexus/scorefxn.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/misfold_nexus/no_spacers/mh_7" sgrna_mc
+
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/in_vitro/mh_7.yml -n10000 inputs/theo_aptamer.yml inputs/auto_anneal.yml inputs/random_spacers.yml,OUTPUT_PREFIX=results/in_vitro/random_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/in_vitro/mh_7.yml -n10000 inputs/theo_aptamer.yml inputs/auto_anneal.yml inputs/example_spacers.yml,OUTPUT_PREFIX=results/in_vitro/example_spacers/mh_7" sgrna_mc
+qsub -t 1-25 -v "ADDAPT_ARGS=inputs/in_vitro/mh_7.yml -n10000 inputs/theo_aptamer.yml inputs/auto_anneal.yml,OUTPUT_PREFIX=results/in_vitro/no_spacers/mh_7" sgrna_mc
