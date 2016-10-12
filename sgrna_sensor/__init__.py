03d12b395d966d0ac519db5508c23df88b954e2a
Kale Kundert <kale@thekunderts.net>
2016-10-12 11:19:20 -0700

    Move code out of __init__.py

--- sgrna_sensor/__init__.py
+++ sgrna_sensor/__init__.py
@@ -2,66 +2,9 @@
 
 __version__ = '0.0.0'
 
-from .components import *
-from .designs import *
 from .helpers import *
 from .sequence import *
-
-def from_name(name, **kwargs):
-    import re, inspect
-
-    name = name.strip()
-    if not name:
-        raise ValueError("Can't parse empty name.")
-
-    tokens = re.findall('[a-zA-Z0-9]+', name)
-
-    # If the first token matches the name of one of the known targeting 
-    # sequences, use that sequence to build the design.  If the next token 
-    # (which would be the first if no targeting sequence was found and the 
-    # second otherwise) matches the name of one of the known ligand, use that 
-    # ligand's aptamer to build the design.
-
-    try:
-        spacer(tokens[0])
-    except ValueError:
-        pass
-    else:
-        if 'target' not in kwargs:
-            kwargs['target'] = tokens.pop(0)
-
-    try:
-        aptamer(tokens[0])
-    except ValueError:
-        pass
-    else:
-        if 'ligand' not in kwargs:
-            kwargs['ligand'] = tokens.pop(0)
-
-    # The first token after the (optional) aptamer specifies the factory 
-    # function to use and must exist in the global namespace. 
-
-    try:
-        factory = globals()[tokens[0]]
-    except KeyError:
-        raise ValueError("No designs named '{}'.".format(tokens[0]))
-
-    # All further tokens are arguments.  Arguments that look like integers need 
-    # to be casted to integers.
-
-    def cast_if_necessary(x):  # (no fold)
-        try: return int(x)
-        except: return x
-
-    args = [cast_if_necessary(x) for x in tokens[1:]]
-
-    # Use keyword arguments passed into this function if the factory knows how 
-    # to handle them.  Silently ignore the arguments otherwise.
-
-    argspec = inspect.getargspec(factory)
-    known_kwargs = {k:v for k,v in kwargs.items() if k in argspec.args}
-    return factory(*args, **known_kwargs)
-
-def molecular_weight(name, polymer='rna'):
-    return from_name(name).mass(polymer)
+from .components import *
+from .designs import *
+from .usage import *
 
