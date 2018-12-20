8445ea15af737b12e17d63a5aff904285e93524e
Kale Kundert <kale@thekunderts.net>
2018-12-19 16:19:58 -0800

    Update 20161013_design_qh_libraries

--- jupyter_notebook/20161013_design_qh_libraries/design_qh_primers.sh
+++ jupyter_notebook/20161013_design_qh_libraries/design_qh_primers.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 ./clone_into_sgrna.py   \
     tpp/qh/3            \
     tpp/qh/4            \
+    add/qh/3            \
+    add/qh/4            \
     -b on               \
     | tee qh_primers.txt
 
