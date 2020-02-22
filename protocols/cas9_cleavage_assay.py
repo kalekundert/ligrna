843963973c34c4976f5adfbd4d03f5f1d0344423
Kale Kundert <kale@thekunderts.net>
2020-02-21 19:59:57 -0500

    Use stepwise.

--- protocols/cas9_cleavage_assay.py
+++ protocols/cas9_cleavage_assay.py
@@ -16,14 +16,14 @@ Options:
         Use the 500 bp target DNA instead of the 4 kb target DNA.
 
     -B --big-rxns
-        Make each reaction 30 μL rather than 15 μL, as recommended by NEB.
+        Make each reaction 30 µL rather than 15 µL, as recommended by NEB.
 
     -f --fresh-theo
         Include a step for preparing fresh theophylline, if you've run out of
         frozen stocks.
 
-    -C --cas9-stock-conc <μM>           [default: 20.0]
-        The stock Cas9 concentration (in μM).
+    -C --cas9-stock-conc <µM>           [default: 20.0]
+        The stock Cas9 concentration (in µM).
 
     -c --cas9-conc <fold>               [default: 1.0]
         The working concentration of Cas9 will be the standard amount times 
@@ -48,12 +48,12 @@ Options:
     -x --extra <percent>                [default: 10]
         How much extra master mix to create.
 
-    -n --notes
-        Print notes on miscellaneous details of the protocol.
+    -N --hide-notes
+        Hide notes on miscellaneous details of the protocol.
 """
 
 import docopt
-import dirty_water
+import stepwise
 
 ## Parse the command line arguments.
 
@@ -64,46 +64,46 @@ num_sgrnas = num_reactions // 2
 
 ## Calculate how much of each reagent will be needed.
 
-cas9_rxn = dirty_water.Reaction('''\
-Reagent        Conc  Each Rxn  Master Mix
-==========  =======  ========  ==========
-water                 10.1 μL         yes
-buffer          10x    3.0 μL         yes
-Cas9           1 μM    0.9 μL         yes
-ligand        30 mM   10.0 μL
-sgRNA       1500 nM    3.0 μL
-DNA           30 nM    3.0 μL
+cas9_rxn = stepwise.MasterMix.from_text('''\
+Reagent       Stock      Volume  MM?
+==========  =======  ==========  ===
+water                to 30.0 µL  yes
+buffer          10x      3.0 µL  yes
+Cas9           1 µM      0.9 µL  yes
+ligand        30 mM     10.0 µL
+sgRNA       1500 nM      3.0 µL
+DNA           30 nM      3.0 µL
 ''')
 
 cas9_rxn.num_reactions = eval(args['<reactions>'])
-cas9_rxn.extra_master_mix = args['--extra']
-cas9_rxn['Cas9'].stock_conc = args['--cas9-stock-conc']
-cas9_rxn['Cas9'].conc *= float(args['--cas9-conc'])
-cas9_rxn['ligand'].conc *= float(args['--ligand-conc'])
-cas9_rxn['sgRNA'].conc *= float(args['--sgrna-conc'])
-cas9_rxn['DNA'].conc *= float(args['--dna-conc'])
-cas9_rxn['DNA'].stock_conc = float(args['--dna-stock-conc'])
+cas9_rxn.extra_percent = eval(args['--extra'])
+cas9_rxn['Cas9'].hold_conc.stock_conc = float(args['--cas9-stock-conc']), 'µM'
+cas9_rxn['Cas9'].hold_stock_conc.conc *= float(args['--cas9-conc'])
+cas9_rxn['ligand'].hold_stock_conc.conc *= float(args['--ligand-conc'])
+cas9_rxn['sgRNA'].hold_stock_conc.conc *= float(args['--sgrna-conc'])
+cas9_rxn['DNA'].hold_stock_conc.conc *= float(args['--dna-conc'])
+cas9_rxn['DNA'].hold_conc.stock_conc = float(args['--dna-stock-conc']), 'nM'
 
-kag_rxn = dirty_water.Reaction('''\
-Reagent       Conc  Each Rxn  Master Mix
-============  ====  ========  ==========
-Orange G        6x   5.64 μL         yes
-RNase A       200x   0.18 μL         yes
-Proteinase K  200x   0.18 μL         yes
+kag_rxn = stepwise.MasterMix.from_text('''\
+Reagent       Stock      Volume  MM?
+============  =====  ==========  ===
+Orange G         6x  to 6.00 µL  yes
+RNase A        200x     0.18 µL  yes
+Proteinase K   200x     0.18 µL  yes
 ''')
 
 kag_rxn.num_reactions = num_reactions
 kag_rxn.show_each_rxn = False
 kag_rxn.show_totals = False
-kag_rxn.extra_master_mix = 3 * float(args['--extra'])
+kag_rxn.extra_percent = 3 * cas9_rxn.extra_percent
 
 if not args['--big-rxns'] and not args['--short-dna']:
-    cas9_rxn.volume /= 2
-    kag_rxn.volume /= 2
+    cas9_rxn.hold_ratios.volume /= 2
+    kag_rxn.hold_ratios.volume /= 2
 
 ## Setup the reagents.
 
-protocol = dirty_water.Protocol()
+protocol = stepwise.Protocol()
 
 if args['--fresh-theo']:
     factor = 1e6 / 180.164 / 30
@@ -113,7 +113,7 @@ Prepare a 30 mM solution of theophylline:
 Reagent          Amount
 ───────────────────────
 theophylline   x≈7.0 mg
-water         {factor:.1f}x μL
+water         {factor:.1f}x µL
 
 Incubate at 50°C to dissolve."""
 
@@ -122,18 +122,18 @@ Incubate at 50°C to dissolve."""
 if using_robot:
     # Prepare the Cas9 master mix.
 
-    protocol += """\
+    protocol += f"""\
 Prepare the Cas9 master mix [1]:
 
 {cas9_rxn}"""
 
     # Put everything into the robot.
 
-    protocol += """\
+    protocol += f"""\
 Load the "kyleb/Cas9 Basic Controls" method and 
 setup the robot's worktable:
 
-A2: 50 μL filter tips.
+A2: 50 µL filter tips.
 
 B1: Reagents and master mixes in a tube rack:
     19: water
@@ -141,19 +141,19 @@ B1: Reagents and master mixes in a tube rack:
     21: Cas9 master mix
     22: target DNA
 
-B2: 50 μL filter tips.
+B2: 50 µL filter tips.
 
 C1: Empty PCR tubes for each reaction in a 96-well 
     thermoblock.  Fill from the top left down.
 
 C2: sgRNAs in a plastic 96-well rack with a 2 mm 
     cardboard support underneath.  There must be 
-    at least 15 μL of each sgRNA.  Fill from the 
+    at least 15 µL of each sgRNA.  Fill from the 
     top left down, skipping every other column."""
 
     # Setup the Cas9 reactions.
 
-    protocol += """\
+    protocol += f"""\
 Run the method.  The robot will setup all the 
 reactions.  Answer its questions as follows:
 
@@ -178,40 +178,40 @@ pipetted for each step."""
 else:
     # Setup the Cas9 reactions.
 
-    protocol += """\
+    protocol += f"""\
 Setup {num_reactions} Cas9 reactions [1]:
 
 {cas9_rxn}
 
-- Add {cas9_rxn[ligand].volume_str} water or ligand to each reaction.
+- Add {cas9_rxn['ligand'].volume} water or ligand to each reaction.
 
-- Add {cas9_rxn[sgRNA].volume_str} sgRNA to each reaction.
+- Add {cas9_rxn['sgRNA'].volume} sgRNA to each reaction.
 
 - Refold the sgRNA by incubating:
   - 95°C for 3 min
   - 4°C for 1 min
 
-- Add {cas9_rxn[master mix].volume_str} Cas9 master mix to each reaction.
+- Add {cas9_rxn.master_mix_volume} Cas9 master mix to each reaction.
 
 - Incubate at room temperature for 10 min.
 
-- Add {cas9_rxn[DNA].volume_str} DNA to each reaction [2].
+- Add {cas9_rxn['DNA'].volume} DNA to each reaction [2].
   
 - Pipet to mix."""
 
 ## Run the Cas9 reactions.
 
-protocol += """\
+protocol += f"""\
 Incubate at 37°C for 1 hour (thermocycler)."""
 
 ## Quench the Cas9 reactions.
 
-protocol += """\
-Add {kag_rxn.volume:.1f} μL 6x KAG master mix to each reaction:
+protocol += f"""\
+Add {kag_rxn.volume:.1f} µL 6x KAG master mix to each reaction:
     
 {kag_rxn}"""
 
-protocol += """\
+protocol += f"""\
 Incubate the reactions at 37°C for 20 min, then at 
 55°C for 20 min, then hold at 12°C (thermocycler)."""
 
@@ -224,46 +224,48 @@ else:
     gel_load = 18
     gel_percent = 1
 
-protocol += """\
-Load the entire reaction ({} μL) on a {}% agarose 
-gel and run at 4.5 V/cm for 70 min [3].""".format(gel_load, gel_percent)
+protocol += f"""\
+Load the entire reaction ({gel_load} µL) on a {gel_percent}% agarose 
+gel and run at 4.5 V/cm for 70 min [3]."""
 
 ## Print the protocol.
 
+protocol.footnotes[1] = """\
+Product numbers:
+Cas9:   NEB M0386T
+buffer: NEB B0386A
+"""
+
+protocol.footnotes[2] = """\
+Be sure to mix the DNA (e.g. by flicking) 
+after it thaws.  The DNA doesn't freeze 
+evenly, so if you don't do this, you may get 
+noticeably different amounts of DNA in 
+different reactions.
+"""
+
+protocol.footnotes[3] = """\
+It really is important to load most of the 
+reaction on the gel and to use a comb that 
+makes thick wells.  I tried loading only 6 µL 
+with the idea that I could use a finer comb 
+and get sharper bands, but the bands were hard 
+to quantify because they were so faint.
+
+For doing lots of reactions, cast a 140 mL 1% 
+agarose/TAE/GelRed in the Owl EasyCast B2 tray 
+with the 25-tooth comb.  Run at 100V for 70 
+min.
+
+For getting publication quality images, cast a 
+140 mL 1% agarose/TAE/GelRed gel in the Owl 
+EasyCast B2 tray with the 20-tooth comb.  Use 
+2 µL of ladder and run at 85V for 90 min.
+"""
+
+if args['--hide-notes']:
+    protocol.clear_footnotes()
+
 print(protocol)
-
-## Notes
-
-if args['--notes']:
-    print("""\
-
-Notes
-─────
-[1] Product numbers:
-    Cas9:   NEB M0386T
-    buffer: NEB B0386A
-
-[2] Be sure to mix the DNA (e.g. by flicking) 
-    after it thaws.  The DNA doesn't freeze 
-    evenly, so if you don't do this, you may get 
-    noticeably different amounts of DNA in 
-    different reactions.
-
-[3] It really is important to load most of the 
-    reaction on the gel and to use a comb that 
-    makes thick wells.  I tried loading only 6 μL 
-    with the idea that I could use a finer comb 
-    and get sharper bands, but the bands were hard 
-    to quantify because they were so faint.
-
-    For doing lots of reactions, cast a 140 mL 1% 
-    agarose/TAE/GelRed in the Owl EasyCast B2 tray 
-    with the 25-tooth comb.  Run at 100V for 70 
-    min.
-   
-    For getting publication quality images, cast a 
-    140 mL 1% agarose/TAE/GelRed gel in the Owl 
-    EasyCast B2 tray with the 20-tooth comb.  Use 
-    2 μL of ladder and run at 85V for 90 min.""")
 
 # vim: tw=50
